4a336d4f8a56c004a93261ba62c33ca815af8d0e
Nadirsha Aliyar <42641677+nadirshaaliyar@users.noreply.github.com>
2021-01-31 18:54:28 +0530

    Update connect.php

--- admin/connect.php
+++ admin/connect.php
@@ -1,8 +1,8 @@
 <?php
 
-	$dsn = 'mysql:host=localhost;dbname=shop';
-	$user = 'root';
-	$pass = '';
+	$dsn = 'mysql:host= remotemysql.com;dbname=PqaTu0evkE';
+	$user = 'PqaTu0evkE';
+	$pass = 'nkzN970fqw';
 	$option = array(
 		PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
 	);
